6c3ad1386e4c449a2ebd86a6dfe53f95e58080e8
clovelCed <cedric.coder@gmail.com>
2016-05-06 00:46:07 +0200

    Add HomePage

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import {Page} from 'ionic-angular';
+import {DatePicker, LocalNotifications} from 'ionic-native';
 
 
 @Page({
@@ -6,8 +7,40 @@ import {Page} from 'ionic-angular';
 })
 export class HomePage {
 
+  at:Date;
+  formatedDate:string;
+  id:number;
+  text:string;
+  title:string;
+  every:string;
+  
   constructor() {
-    // app.getComponent('leftMenu').enable(true);
+
+  }
+
+  openDatePicker(){
+    DatePicker.show({
+      date:new Date(),
+      minDate:new Date(),
+      mode:'datetime',
+      titleText:'Start notification'
+    }).then(at => this.formatedDate = this.formatDate(at));
+  }
+
+  formatDate(date:Date){
+    this.at = date;
+    var options = { weekday: "long", year: "numeric", month: "short", day: "numeric", hour:"numeric", minute:"numeric" };
+    return new Intl.DateTimeFormat("fr-FR", options).format(date);
+  }
+
+  notify(){
+    LocalNotifications.schedule({
+      id:this.id,
+      title:this.title,
+      text:this.text,
+      at:this.at,
+      every:this.every
+    });
   }
 
 }
