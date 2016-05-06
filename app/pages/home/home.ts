b7104e01ea4cd418660306eedfa165f3249f319d
clovelCed <cedric.coder@gmail.com>
2016-05-06 02:27:54 +0200

    Add clear and delete notifications

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,5 +1,6 @@
 import {Page, Toast, NavController} from 'ionic-angular';
 import {DatePicker, LocalNotifications} from 'ionic-native';
+import {FormBuilder, Validators} from 'angular2/common';
 
 
 @Page({
@@ -8,15 +9,20 @@ import {DatePicker, LocalNotifications} from 'ionic-native';
 export class HomePage {
 
   at:Date;
-  id:number;
-  text:string;
-  title:string;
   every:string;
   formatedDate:string;
   nav:NavController;
+  loginForm:any;
 
-  constructor(nav: NavController) {
+  constructor(nav: NavController, form: FormBuilder) {
     this.nav = nav;
+
+    this.loginForm = form.group({
+      id:["", Validators.required],
+      text:["", Validators.required],
+      title:["", Validators.required]
+
+    });
   }
 
   openDatePicker(){
@@ -34,19 +40,35 @@ export class HomePage {
     return new Intl.DateTimeFormat("fr-FR", options).format(date);
   }
 
-  notify(){
+  notify(event){
     LocalNotifications.schedule({
-      id:this.id,
-      title:this.title,
-      text:this.text,
+      id:this.loginForm.value.id,
+      title:this.loginForm.value.title,
+      text:this.loginForm.value.text,
       at:this.at,
       every:this.every
     });
 
     this.nav.present(Toast.create({
-      message:'Notification programmée',
+      message:'Programmed notification !',
       duration:3000
     }));
+  }
+
+  cancelById(){
+    LocalNotifications.cancel(this.loginForm.value.id)
+      .then(() => this.nav.present(Toast.create({
+        message:'Notification '+this.loginForm.value.id+' deleted !',
+        duration:3000
+      })));
+  }
+
+  clearAll(){
+    LocalNotifications.clearAll()
+    .then(() => this.nav.present(Toast.create({
+      message:'All notifications cleared !',
+      duration:3000
+    })));
   }
 
 }
